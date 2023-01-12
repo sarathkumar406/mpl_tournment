bf5d9f72747d59744c9fdffab04351e2613871c7
sarathkumar.406@gmail.com <sarathkumar.406@gmail.com>
2023-01-12 23:05:05 +0530

    Initial Commit

---
+++ assets/data/2022.js
@@ -0,0 +1,390 @@
+let winners = {
+    name:'Team Winners',
+    image:'assets/img/team/winners.png'
+}
+let runners = {
+    name:'Team Runners',
+    image:'assets/img/team/Runners.png'
+}
+let teamsList = [
+    {
+        team_id:1,
+        team_name: 'Team Srikanth'
+    },{
+        team_id:2,
+        team_name: 'Team Yeshwanth'
+    },{
+        team_id:3,
+        team_name: 'Team Sai Kiran'
+    },{
+        team_id:4,
+        team_name: 'Chandu Choobies'
+    },{
+        team_id:5,
+        team_name: 'Team Sandeep'
+    }
+]
+let playersList=[
+	{
+		player_name: 'Madana Srikanth - Captain',
+		player_pic: 'assets/img/team/Srikanth.png',
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Sajja LakshmiKanth',
+		player_pic: 'assets/img/team/kanth.png',
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Y. Siva Subramanyam',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Madana Venkatesh',
+		player_pic: 'assets/img/team/Konda.png',
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Gowrabathina Prasanth',
+		player_pic: 'assets/img/team/Prasanth.png',
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Gowrabathina Mahesh',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Muchi Sreenu',
+		player_pic: 'assets/img/team/SreenuMuchi.png',
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Sajja SivaKumar',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'K Rakesh',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Mahesh',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Subbarayalu',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+		player_name: 'Siva Sai',
+		player_pic: null,
+		team_id: 1,
+		team_name: 'Team Srikanth'
+	},
+	{
+          player_name: 'Yeshwanth - Captain',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Teja',
+          player_pic: 'assets/img/team/Teja_prev_ui.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Kasa Venkatesh',
+          player_pic: 'assets/img/team/Abbodu.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Sapram Eshwaraiah (Dhawan)',
+          player_pic: 'assets/img/team/Shikar.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Thupati Bhadra',
+          player_pic: 'assets/img/team/Bhadra.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Kompala Muni Pavan',
+          player_pic: 'assets/img/team/MuniPavan.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Sajja Venkatesh (Chinna Tenkay)',
+          player_pic: 'assets/img/team/ChinnaTenkay.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Karthik',
+          player_pic: 'assets/img/team/Karthik.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Shekar (Bolt)',
+          player_pic: null,
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Marem Sai Leela Mohan',
+          player_pic: 'assets/img/team/LeelaKrishna.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Rohit Sharma',
+          player_pic: null,
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+      {
+          player_name: 'Sudheer',
+          player_pic: 'assets/img/team/Sudheer.png',
+          team_id: 2,
+          team_name:'Team Yeshwanth'
+      },
+       {
+        player_name: 'Sai Kiran - Captain',
+        player_pic: 'assets/img/team/saikiran.png',
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Challa Sai Kumar',
+        player_pic: 'assets/img/team/ChSai.png',
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Jakkala Subbarao',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Sajja Sravan Kumar (Kaatre)',
+        player_pic: 'assets/img/team/Kaatre.png',
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Yarasi Rajesh (Innapetti)',
+        player_pic: 'assets/img/team/YRajesh.png',
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Challa Venkateshwarlu',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Shaik Asif',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Ranga',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Balaji (Dubai)',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Gowrabathina Balu',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+    {
+        player_name: 'Ram Charan',
+        player_pic: null,
+        team_id: 3,
+        team_name:'Team Sai Kiran'
+    },
+     {
+        player_name: 'Chandu - Captain',
+        player_pic: 'assets/img/team/chandhu.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sapram Eswaraiah (Chico)',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Madana Balaji',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Lokesh',
+        player_pic: 'assets/img/team/lokeshkodi.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Srinivasulu ( Pedda Tenkay )',
+        player_pic: 'assets/img/team/PeddaTenkay.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Veerabhadraiah',
+        player_pic: 'assets/img/team/sajjaveerabhadra.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sai Sajja',
+        player_pic: 'assets/img/team/chenchusai.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Balaji',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Madana Balaji (Jendapuram)',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Rajesh',
+        player_pic: 'assets/img/team/SajjaRajesh.png',
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Bommini Srinuvasulu',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+        player_name: 'Sajja Mohan Kumar',
+        player_pic: null,
+        team_id: 4,
+        team_name:'Chandu Choobies'
+    },
+     {
+           player_name: 'Sandeep - Captain',
+           player_pic: 'assets/img/team/Sandeep.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+     },
+    {
+           player_name: 'Kompala Lokesh',
+           player_pic: 'assets/img/team/Sathi.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Macha Venkatesh',
+           player_pic: 'assets/img/team/Kadapa.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Sajja Venkatesh (Harry)',
+           player_pic: 'assets/img/team/Venky.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Hemanth',
+           player_pic: 'assets/img/team/Hemanth.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Nakkal Sai',
+           player_pic: 'assets/img/team/NakkalSai.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Gowrabhathina Sravan Kumar',
+           player_pic: null,
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Vishnu',
+           player_pic: 'assets/img/team/vishnu.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Sajja Sai (Left Hand)',
+           player_pic: 'assets/img/team/leftSai.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Balaji (Dish)',
+           player_pic: null,
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Muchi Uday (Chinna Muchi)',
+           player_pic: null,
+           team_id: 5,
+           team_name:'Team Sandeep'
+       },
+       {
+           player_name: 'Jakkala Devendra',
+           player_pic: 'assets/img/team/Devendra.png',
+           team_id: 5,
+           team_name:'Team Sandeep'
+       }
+       	
+	
+]
